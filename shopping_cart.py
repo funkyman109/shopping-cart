2a6b706883968f9eb50e9778a002bca86d80795a
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-02-09 15:22:09 -0500

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -25,7 +25,25 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    Source: https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/datatypes/numbers.md#formatting-as-currency
+    Param: my_price (int or float) like 4000.444444
+    Example: to_usd(4000.444444)
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}" #> $12,000.71
+
+
 print(products)
 # pprint(products)
 
 # TODO: write some Python code here to produce the desired output
+
+#info capture
+selected_id= input("please input product identifier:")
+print(selected_id)
+print(type(selected_id))
+
+#info display/output
