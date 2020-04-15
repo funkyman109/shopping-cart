c2ed7f61514de5be6bffe6600c6d789a2b03adb7
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-04-15 17:13:36 -0400

    Update shop_test.py

--- test/shop_test.py
+++ test/shop_test.py
@@ -1,4 +1,6 @@
-from app.shopping_cart import to_usd, tax
+from datetime import datetime
+from freezegun import freeze_time
+from app.shopping_cart import to_usd, tax, find_product
 
 def test_to_usd():
     result = to_usd(3.47)
@@ -7,4 +9,25 @@ def test_to_usd():
 def test_tax():
     result= tax(40)
     assert result == 3.5
+
+#taken from https://pypi.org/project/freezegun/
+@freeze_time("2012-01-14")
+def test_time():
+    result = datetime.now()
+    assert result == datetime(2012, 1, 14)
+
+def test_find_product():
     
+    products = [
+        {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+    ]
+    selected_ids = []
+    selected_id = 3  
+    selected_ids.append(selected_id)
+    total_price = 0
+    for i in selected_ids:
+        result = find_product(i, total_price, products,selected_id)
+    
+    assert result == 2.49
+
+
